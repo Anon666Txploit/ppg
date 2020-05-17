bcfbdd6c009239f93ad39ce35bd83ec39844a3a2
Anon666Txploit <63142171+Anon666Txploit@users.noreply.github.com>
2020-05-17 09:55:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<html lang="en-US"><head><title>Own3d By ./Anon666Txploit</title>
+<meta property="og:title" content="Own3d By ./Anon666Txploit">
+<meta property="og:image" content="" />
+<meta property="og:image:secure_url" content="" />
+<link rel="icon" type="image/png" href="https://i.ibb.co/ZxWRwmm/images-6.jpg" type="image/x-icon"/>
+<link rel="icon" type="image/png" href="https://i.ibb.co/ZxWRwmm/images-6.jpg"><link rel="shortcut icon">
+<meta property="og:description" content="~[+] Hunter Force Exploit [+]~">
+<meta property="og:author" content="[+] Hunter Force Exploit [+]">
+<meta charset="UTF-8">
+<meta charset="UTF-8">
+
+
+<style>body{background:#000;
+       cursor: url(http://cur.cursors-4u.net/cursors/cur-9/cur862.ani), url(http://cur.cursors-4u.net/cursors/cur-9/cur862.png), progress !important;
+}
+
+
+ </style>
+
+
+</head><body oncontextmenu="return false" onkeydown="return false" onmousedown="return false"><br><br><br>
+<center><img style="width:340px;" src="https://i.ibb.co/hDYMBQM/1588956778-picsaycccf.png">
+      
+      
+	  
+      <br><br><font color="white" face="courier new" font size="5">Own3d By ./Anon666Txploit</font>
+      <pre>
+      	<center><font face="courier" color="white">massage:</font></center>
+      <br><br><font color="white" face="courier new">Kehebatan Seseorang Tidak di Ukur <br>Dari Seberapa Banyak yang Telah 
+Dia Perbuat,Tapi di Ukur Dari <br> Seberapa Banyak Ilmu Yang Telah Dia Bagikan.</font><br><font color="white" face="courier">
+</font>
+     	<center><font face="courier" color="white">Greetz:</font></center>
+     	<center><font face="courier" color="red">[+] Hunter Force Exploit ~ ALL Defacer Indonesia [+]</font></center>
+ </center></body></html>
